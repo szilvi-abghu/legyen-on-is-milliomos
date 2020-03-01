e817288d1c50c74ece187b30e8f2d92a701701fd
szilvi.abghu <zs.sziszi@gmail.com>
2020-03-01 16:49:34 +0100

    2.0

--- LegyOnIsMilliomosSzoftverfejleszto/Program.cs
+++ LegyOnIsMilliomosSzoftverfejleszto/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.IO; //0. lépés
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace LegyOnIsMilliomosSzoftverfejleszto
 {
-    struct Kerdes
+    struct Kerdes //1.lépés
     {
         public string kategoria;
         public string kerdes;
@@ -26,78 +26,117 @@ namespace LegyOnIsMilliomosSzoftverfejleszto
     class Program
     {
         static Random rnd = new Random();
-        static List<Kerdes> teszt;
+        static List<Kerdes> teszt; //2.lépés
+        static int[] nyeremény = { 0, 100, 1000, 2000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000 };
+        static int kerdesekSzama = 3;
+        static bool ok = true;
+
         static void Main(string[] args)
         {
-            Beolvas();
-            Kiiras();
+            Beolvas(); // 3. lépés
+           
+            Kiiras(); //4. lépés
 
             //Console.ReadKey();
         }
 
+        
         private static void Kiiras()
         {
             
-            for (int i = 1; i < teszt.Count; i++)
+            for (int i = 1; i < (kerdesekSzama+1); i++) // 5.lépés: i=1-től indítva, mert a 0 a táblázat fejléce
             {
-                FeladatMegjelenitese(i);
-               
-                
+                if (ok) // ha ok=true, azaz mindaddig, amíg nem rontja el a feladatot
+                {
+                    FeladatMegjelenitese(i);
+                }
+
+                else // ha ok=false, azaz amikor elrontja a feladatot(hiszen ebben az esetben átváltjuk ok értékét false-ra)
+                { 
+                    break;
+                }
 
 
-                //Console.WriteLine("Folytatáshoz nyomjon ENTER-t...");
-                //while (Console.ReadKey().Key != ConsoleKey.Enter)
-                //{
-                //    Console.Write("\b \b"); //amíg nem enter üt le a felhasználó, addig backslash space backslash-sel visszatörlődik a beírt karakter
-                //}
+
+            }
+
+            if (ok)
+            {
+                Kiertekeles();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine(  "Köszönöm a játékot, ma csak tapasztalatot nyertél!\n" +
+                                    "Kilépéshez nyomd le az Esc billenytűt!");
+                while (Console.ReadKey().Key != ConsoleKey.Escape)
+                    Console.Write("\b \b");
 
             }
             
+
+        }
+
+        private static void Kiertekeles()
+        {
+            
+            Console.WriteLine("Gratulálok, minden kérdésre tudtad a választ!\n" +
+                              "Nyereményed még egyszer: {0, 0:N0} Ft\n" +
+                              "Ha van kedved, játsz újra!\n" +
+                              "Kilépéshez nyomd le az Esc billenytűt!", nyeremény[kerdesekSzama]);
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+                    Console.Write("\b \b");
         }
 
         private static void FeladatMegjelenitese(int i)
         {
-           Valasz[] valaszok = new Valasz[4];
+            //List<Kerdes> kevertTeszt = ListaKever(teszt);
+            int randomindex = rnd.Next(1, teszt.Count);
+            Valasz[] valaszok = new Valasz[4];
             valaszok[0] = new Valasz()
             {
-                valaszSzovege = teszt[i].helyesValasz,
+                valaszSzovege = teszt[randomindex].helyesValasz,
                 helyesValasz = true,
             };
             valaszok[1] = new Valasz()
             {
-                valaszSzovege = teszt[i].rosszValasz1,
+                valaszSzovege = teszt[randomindex].rosszValasz1,
                 helyesValasz = false,
             };
             valaszok[2] = new Valasz()
             {
-                valaszSzovege = teszt[i].rosszValasz2,
+                valaszSzovege = teszt[randomindex].rosszValasz2,
                 helyesValasz = false,
             };
             valaszok[3] = new Valasz()
             {
-                valaszSzovege = teszt[i].rosszValasz3,
+                valaszSzovege = teszt[randomindex].rosszValasz3,
                 helyesValasz = false,
             };
 
             Valasz[] kevertValaszok = TombKever(valaszok);
 
-            Console.WriteLine($"{(i)}. kérdés: {teszt[i].kategoria} kategóriában\n");
-            Console.WriteLine($"{teszt[i].kerdes}");
+            
+
+            Console.WriteLine($"10/{i}. kérdés: {teszt[randomindex].kategoria} kategóriában\n"); //kategória és kérdés és lehetséges válaszok kiírása
+            Console.WriteLine($"{teszt[randomindex].kerdes}");
             Console.WriteLine(  $"\ta) {kevertValaszok[0].valaszSzovege}\n" +
                                 $"\tb) {kevertValaszok[1].valaszSzovege}\n" +
                                 $"\tc) {kevertValaszok[2].valaszSzovege}\n" +
                                 $"\td) {kevertValaszok[3].valaszSzovege}\n");
+            teszt.RemoveAt(randomindex);
                                    
-            Console.WriteLine("Írd a helyes válasz betűjelét!");
+            Console.WriteLine("Írd a helyes válasz betűjelét, és nyomj egy enter-t!");
             //while (Console.ReadKey().Key != ConsoleKey.A && Console.ReadKey().Key != ConsoleKey.B && Console.ReadKey().Key != ConsoleKey.C && Console.ReadKey().Key != ConsoleKey.D && Console.ReadKey().Key != ConsoleKey.Enter)
             //{
             //    Console.Write("\b \b"); //amíg nem enter üt le a felhasználó, addig backslash space backslash-sel visszatörlődik a beírt karakter
             //}
-            char valasz = char.Parse(Console.ReadLine());
 
             
-
+            char valasz = char.Parse(Console.ReadLine());
+                      
             char[] valaszBetujel = { 'a', 'b', 'c', 'd' };
+
             int valaszindex = Array.IndexOf(valaszBetujel, valasz);
 
             //Console.WriteLine($"{valaszindex}");
@@ -111,25 +150,35 @@ namespace LegyOnIsMilliomosSzoftverfejleszto
             else if (kevertValaszok[valaszindex].helyesValasz == true)
             {
                 Console.WriteLine("Helyes válasz!");
-                Thread.Sleep(3000);
+                Console.WriteLine("Nyereményed: {0, 0:N0} Ft", nyeremény[i]);
+                Thread.Sleep(1000);
                 Console.Clear();
+                
             }
             else
             {
+                ok = false;
                 Console.WriteLine("Helytelen válasz!\n");
                 Thread.Sleep(3000);
-                Console.Clear();
-                Console.WriteLine("Köszönöm a játékot, ma csak tapasztalatot nyertél!");
             }
-
-            //while (Console.ReadKey().Key != ConsoleKey.Escape)
-                //Console.Write("\b \b");
-
-
-
-
+                       
         }
 
+
+        private static List<Kerdes> ListaKever(List<Kerdes> teszt)
+        {
+            for (int i = 0; i < teszt.Count; i++)
+            {
+                int x = rnd.Next(teszt.Count);
+                int y = rnd.Next(teszt.Count);
+
+                Kerdes cs = teszt[x];//letárolom egy csereváltozóban
+                teszt[x] = teszt[y];
+                teszt[y] = cs;
+
+            }
+            return teszt;
+        }
         private static Valasz[] TombKever(Valasz[] valaszok)
         {
             for (int i = 0; i < valaszok.Length; i++)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # legyen-on-is-milliomos
+
+- megfelelõ adatokat (kategóriákat, kérdéseket, 1 helyes választ és 3 rossz választ) tartalmazó csv file létrehozása
+- a program beolvassa a file-t, és létrehoz listát (teszt néven), amely struktúrába (Kérdés struct) rendezve tartalmazza a sorokat (kategória, kérdés, válaszokat)
+- a 4 választ tömbbe (válasz tömb néven) rendezzük, amely struktúrába (Válasz struct ) rendezve tartalmazza a sorokat (válasz szövege, helyességére vonatkozó információk)
+- a válaszokat összekeverjük, és egy kevert válaszok tömbbe rendezzük
+- kiírjuk a feladatot:
+	kérdéseket
+	kevert válaszokat (a,b,c,d-vel jelölve)
+- kérjük a felhasználót, hogy válaszoljon
+- beolvassuk a válaszát
+- kinyerjük a válasz indexét (letároljuik valaszindex néven)
+- vizsgáljuk, hogy a kevertValaszok valaszindexedik eleme igaz-e
+	ha igaz, kiírjuk, hogy helyes válasz és 3 mp múlva töröljük a konzolt, és a for ciklus megy tovább a következõ kérdésre
+	ha hamis, vége a játéknak
